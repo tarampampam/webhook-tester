bcaf7c29bb5a4f6f68c691c40f6bdd14af84f1f8
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2022-04-17 20:49:52 +0400

    Internal healthcheck in logs disabled (#151)

--- internal/pkg/http/middlewares/logreq/logreq.go
+++ internal/pkg/http/middlewares/logreq/logreq.go
@@ -3,6 +3,7 @@ package logreq
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/felixge/httpsnoop"
 	"github.com/gorilla/mux"
@@ -16,14 +17,16 @@ func New(log *zap.Logger) mux.MiddlewareFunc {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			metrics := httpsnoop.CaptureMetrics(next, w, r)
 
-			log.Info("HTTP request processed",
-				zap.String("remote addr", realip.FromHTTPRequest(r)),
-				zap.String("useragent", r.UserAgent()),
-				zap.String("method", r.Method),
-				zap.String("url", r.URL.String()),
-				zap.Int("status code", metrics.Code),
-				zap.Duration("duration", metrics.Duration),
-			)
+			if !strings.Contains(strings.ToLower(r.UserAgent()), "healthcheck") {
+				log.Info("HTTP request processed",
+					zap.String("remote addr", realip.FromHTTPRequest(r)),
+					zap.String("useragent", r.UserAgent()),
+					zap.String("method", r.Method),
+					zap.String("url", r.URL.String()),
+					zap.Int("status code", metrics.Code),
+					zap.Duration("duration", metrics.Duration),
+				)
+			}
 		})
 	}
 }
