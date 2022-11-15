226e5d43860beb535a1bc3efbe47478f5e369234
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2022-11-15 11:23:55 +0400

    Added code snippets for requests for different programming languages

--- web/src/index.ts
+++ web/src/index.ts
@@ -14,13 +14,25 @@ import {
   faPlus,
   faQuestion
 } from '@fortawesome/free-solid-svg-icons'
-import {faGithub} from '@fortawesome/free-brands-svg-icons'
+import {
+  faGithub,
+  faGolang,
+  faJava,
+  faJs,
+  faNodeJs,
+  faPhp,
+  faPython
+} from '@fortawesome/free-brands-svg-icons'
 import {faCopy} from '@fortawesome/free-regular-svg-icons'
 import ClipboardJS from 'clipboard'
 import iziToast from 'izitoast'
 import mainApp from './views/main-app.vue'
 import hljs from 'highlight.js/lib/core'
-import javascript from 'highlight.js/lib/languages/javascript'
+import hlJavascript from 'highlight.js/lib/languages/javascript'
+import hlGo from 'highlight.js/lib/languages/go'
+import hlPython from 'highlight.js/lib/languages/python'
+import hlJava from 'highlight.js/lib/languages/java'
+import hlPhp from 'highlight.js/lib/languages/php'
 import hljsVuePlugin from '@highlightjs/vue-plugin'
 
 library.add( // https://fontawesome.com/icons
@@ -37,9 +49,19 @@ library.add( // https://fontawesome.com/icons
   faAtom,
   faDownload,
   faPersonRunning,
+  faPhp,
+  faGolang,
+  faPython,
+  faJava,
+  faNodeJs,
+  faJs,
 )
 
-hljs.registerLanguage('javascript', javascript)
+hljs.registerLanguage('javascript', hlJavascript)
+hljs.registerLanguage('go', hlGo)
+hljs.registerLanguage('python', hlPython)
+hljs.registerLanguage('java', hlJava)
+hljs.registerLanguage('php', hlPhp)
 
 new ClipboardJS('[data-clipboard]') // <https://clipboardjs.com/#events>
   .on('error', () => {
