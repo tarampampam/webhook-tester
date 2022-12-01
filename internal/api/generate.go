0b1407ced9bf8063c4ff4cf1349f1b1ee0a3dd49
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2022-12-01 12:13:37 +0400

    fix(oapi): prevent deepmap/oapi-codegen removal grom the go.mod file

--- internal/api/generate.go
+++ internal/api/generate.go
@@ -1,4 +1,6 @@
 package api
 
+import _ "github.com/deepmap/oapi-codegen/pkg/types"
+
 // Generate openapi stubs (`oapi-codegen` is required for this):
 //go:generate oapi-codegen -generate server,types -o ./openapi.gen.go -package api ./../../api/openapi.yml
