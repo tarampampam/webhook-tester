221b68da5d0a6063e0aec68112ce1e815f8465fc
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2024-10-16 15:18:49 +0400

    wip: 🔕 temporary commit

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,7 +2,6 @@ package storage
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"net/url"
@@ -74,71 +73,7 @@ type (
 		URL        URL               `json:"url"`         // Uniform Resource Identifier
 		CreatedAt  Time              `json:"created_at"`  // creation time
 	}
+
+	Time struct{ time.Time } // custom type to override serialization
+	URL  struct{ url.URL }   // custom type to override serialization
 )
-
-// --------------------------------------------------------------------------------------------------------------------
-
-// Time is a custom time.Time type that marshals and unmarshals time in unix-nano format.
-type Time struct{ time.Time }
-
-var (
-	_ json.Marshaler   = (*Time)(nil) // ensure that Time implements the json.Marshaler interface
-	_ json.Unmarshaler = (*Time)(nil) // ensure that Time implements the json.Unmarshaler interface
-)
-
-// MarshalJSON implements the json.Marshaler interface and returns the time in unix-nano format.
-func (t Time) MarshalJSON() ([]byte, error) {
-	if t.IsZero() {
-		return []byte("0"), nil
-	}
-
-	return []byte(fmt.Sprintf("%d", t.Time.UnixNano())), nil // fmt.Sprintf used here to avoid exponential notation
-}
-
-// UnmarshalJSON implements the json.Unmarshaler interface and parses the time in unix-nano format.
-func (t *Time) UnmarshalJSON(data []byte) error {
-	var unixNano int64
-	if err := json.Unmarshal(data, &unixNano); err != nil {
-		return err
-	}
-
-	if unixNano == 0 {
-		t.Time = time.Time{}
-
-		return nil
-	}
-
-	t.Time = time.Unix(0, unixNano)
-
-	return nil
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-// URL is a custom url.URL type that marshals and unmarshals URL as a string.
-type URL struct{ url.URL }
-
-var (
-	_ json.Marshaler   = (*URL)(nil) // ensure that URL implements the json.Marshaler interface
-	_ json.Unmarshaler = (*URL)(nil) // ensure that URL implements the json.Unmarshaler interface
-)
-
-// MarshalJSON implements the json.Marshaler interface and returns the URL as a string.
-func (u URL) MarshalJSON() ([]byte, error) { return json.Marshal(u.String()) }
-
-// UnmarshalJSON implements the json.Unmarshaler interface and parses the URL as a string.
-func (u *URL) UnmarshalJSON(data []byte) error {
-	var s string
-	if err := json.Unmarshal(data, &s); err != nil {
-		return err
-	}
-
-	parsed, err := url.Parse(s)
-	if err != nil {
-		return err
-	}
-
-	u.URL = *parsed
-
-	return nil
-}
