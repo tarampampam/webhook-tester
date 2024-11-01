91a799d4e8226d71fae2dcfdb37c1b83c6b51b56
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2024-11-01 21:44:39 +0400

    wip: 🔕 temporary commit

--- web/generate_dist_stub.go
+++ web/generate_dist_stub.go
@@ -18,10 +18,19 @@ func main() {
 		}
 	}
 
-	var indexPath = path.Join(distDir, "index.html")
+	var (
+		indexPath  = path.Join(distDir, "index.html")
+		robotsPath = path.Join(distDir, "robots.txt")
+	)
 
 	if _, err := os.Stat(indexPath); err != nil && errors.Is(err, os.ErrNotExist) {
 		if err = os.WriteFile(indexPath, []byte("<html><!-- generated automatically --></html>\n"), 0644); err != nil {
+			panic(err)
+		}
+	}
+
+	if _, err := os.Stat(robotsPath); err != nil && errors.Is(err, os.ErrNotExist) {
+		if err = os.WriteFile(robotsPath, []byte("User-agent: *\nDisallow: /\n"), 0644); err != nil {
 			panic(err)
 		}
 	}
